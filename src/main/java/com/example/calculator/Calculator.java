e5523ba42ca312b398d1eed5694aab909f8f345b
3uomlkh <3uomlkh@gmail.com>
2025-01-02 23:56:42 +0900

    feat: Lv.1 계산기 구현

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -1,7 +1,41 @@
 package com.example.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+    private static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello, Calculator!");
+        calculate();
+    }
+
+    private static void calculate() {
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+        sc.nextLine();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+        sc.nextLine();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.nextLine().charAt(0);
+
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("올바른 연산자를 입력하세요.");
+        }
+
+        System.out.println("결과: " + result);
     }
 }
