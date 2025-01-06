88c501b48eb41502279ad4803f3c3c637aae7d99
3uomlkh <3uomlkh@gmail.com>
2025-01-06 14:55:08 +0900

    feat: Lv.2 계산기 - 연산자에 따른 사칙연산 수행 후 결과 값을 저장하는 메서드 구현

--- src/main/java/com/example/calculator2/Calculator.java
+++ src/main/java/com/example/calculator2/Calculator.java
@@ -6,6 +6,32 @@ import java.util.List;
 public class Calculator {
     private List<Integer> results = new ArrayList<>();
 
+    public int calculate(int num1, int num2, char operator) {
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = add(num1, num2);
+                break;
+            case '-':
+                result = subtract(num1, num2);
+                break;
+            case '*':
+                result = multiply(num1, num2);
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    break;
+                }
+                result = divide(num1, num2);
+                break;
+            default:
+                System.out.println("올바른 연산자를 입력하세요.");
+        }
+        results.add(result);
+        return result;
+    }
+
     public List<Integer> getResult() {
         return results;
     }
