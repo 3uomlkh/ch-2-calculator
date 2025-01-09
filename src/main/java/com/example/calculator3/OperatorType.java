6dc72847f64f63f34e3e276b82b41b2a20cbe687
3uomlkh <3uomlkh@gmail.com>
2025-01-09 15:01:44 +0900

    refactor: Lv.3 계산기 - char 타입 연산자 추가

--- src/main/java/com/example/calculator3/OperatorType.java
+++ src/main/java/com/example/calculator3/OperatorType.java
@@ -3,18 +3,24 @@ package com.example.calculator3;
 import java.util.function.BiFunction;
 
 public enum OperatorType {
-    ADD((a, b) -> (a + b)),
-    SUBTRACT((a, b) -> (a - b)),
-    MULTIPLY((a, b) -> (a * b)),
-    DIVIDE((a, b) -> {
+    ADD('+', (a, b) -> a + b),
+    SUBTRACT('-', (a, b) -> a - b),
+    MULTIPLY('*', (a, b) -> a * b),
+    DIVIDE('/', (a, b) -> {
         if (b == 0) throw new ArithmeticException("0으로 나눌 수 없습니다.");
         return a / b;
     });
 
+    private final char symbol;
     private final BiFunction<Double, Double, Double> operation;
 
-    OperatorType(BiFunction<Double, Double, Double> operation) {
+    OperatorType(char symbol,BiFunction<Double, Double, Double> operation) {
+        this.symbol = symbol;
         this.operation = operation;
+    }
+
+    public char getSymbol() {
+        return symbol;
     }
 
     public double apply(double a, double b) {
